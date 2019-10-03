16413e62247d3890a5bf2972b74e3f0cd7e99acb
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-10-02 20:49:06 -0400

    Added stuff

--- DrawingShapesStella/main.lua
+++ DrawingShapesStella/main.lua
@@ -4,11 +4,11 @@
 -- This program displays multiple shapes
 
 -- create my local variables
-local textSize = 50
+local textSize = 25
 local myTriangle
 local trianlgeText
-local areaTextObject
-local areaOfTriangle = baseOfTriangle*heightOfTriangle/2
+local areaTextTriangle
+local areaOfTriangle 
 local baseOfTriangle = 8
 local heightOfTriangle = 6
 local myTrapezoid
@@ -21,6 +21,8 @@ local myOctagon
 local octagonText
 local myQuadrilateral
 local quadrilateralText
+
+-- create local variables for the positioning of each shape.
 local triangleV = {135, 90, -135, 90, 0, -90}
 local trapezoidV = {-60, -90, 60, -90, 120, 60, -120, 60}
 local pentagonV = {-120, -30, 0, -90, 120, -30, 60, 60, -60, 60}
@@ -28,17 +30,12 @@ local hexagonV = {-180, 0, -80, -80, 80, -80, 180, 0, 80, 80, -80, 80}
 local octagonV = {-120, 0, -120, -60, -60, -120, 60, -120, 120, -60, 120, 0, 60, 60, -60, 60}
 local quadrilateralV = {-120, -60, -120, -120, 60, -60, 60, 60}
 
--- draw the triangle's location on the screens size. 
-myTriangle = display.newPolygon(175, 200, triangleV)
--- draw the triangle's location on the screens size. 
+-- draw each shape's location on the screens size. 
+myTriangle = display.newPolygon(175, 200, triangleV) 
 myTrapezoid = display.newPolygon(525, 200, trapezoidV)
--- draw the triangle's location on the screens size. 
 myPentagon = display.newPolygon(830, 200, pentagonV)
--- draw the triangle's location on the screens size. 
 myHexagon = display.newPolygon(200, 525, hexagonV)
--- draw the triangle's location on the screens size. 
-myOctagon = display.newPolygon(560, 525, octagonV)
--- draw the triangle's location on the screens size. 
+myOctagon = display.newPolygon(560, 525, octagonV) 
 myQuadrilateral = display.newPolygon(850, 525, quadrilateralV)
 
 -- set the backround colour of my screen. 
@@ -48,7 +45,7 @@ display.setDefault("background", 204/255, 255/255, 229/255)
 -- to remove status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- set the width of the border for the shapes
+-- set the width of the border for each shape.
 myTriangle.strokeWidth = 20
 myTrapezoid.strokeWidth = 20
 myPentagon.strokeWidth = 20
@@ -56,7 +53,7 @@ myHexagon.strokeWidth = 20
 myOctagon.strokeWidth = 20
 myQuadrilateral.strokeWidth = 20
 
--- set the colour of the shapes
+-- set the colour of each shape.
 myTriangle:setFillColor(204/255, 255/255, 255/255)
 myTrapezoid:setFillColor(204/255, 255/255, 204/255)
 myPentagon:setFillColor(255/255, 229/255, 204/255)
@@ -64,7 +61,7 @@ myHexagon:setFillColor(255/255, 204/255, 204/255)
 myOctagon:setFillColor(255/255, 255/255, 204/255)
 myQuadrilateral:setFillColor(255/255, 204/255, 255/255)
 
--- set the color of the border of the shapes
+-- set the color of the border of each shape.
 myTriangle:setStrokeColor(204/255, 204/255, 255/255)
 myTrapezoid:setStrokeColor(204/255, 204/255, 255/255)
 myPentagon:setStrokeColor(204/255, 204/255, 255/255)
@@ -72,7 +69,7 @@ myHexagon:setStrokeColor(204/255, 204/255, 255/255)
 myOctagon:setStrokeColor(204/255, 204/255, 255/255)
 myQuadrilateral:setStrokeColor(204/255, 204/255, 255/255)
 
--- creat text object, set its position and add color of text
+-- creat text object, set its position and add the color of the text.
 local textObject = display.newText ("Triangle", 175, 350, nil, 50)
 textObject:setTextColor (0,0,0)
 local textObject = display.newText ("Trapezoid", 530, 350, nil, 50)
@@ -86,11 +83,12 @@ textObject:setTextColor (0,0,0)
 local textObject = display.newText ("Quadrilateral", 875, 650, nil, 50)
 textObject:setTextColor (0,0,0)
 
-local areaTextObject = display.newText ("The area of the triangle is ... ", 175, 350, nil, 50)
-textObject:setTextColor (0,0,0)
+-- calculate the area of the rectangle
+areaOfTriangle = baseOfTriangle * heightOfTriangle/2
 
--- write the area of the circle on the screen. Take into consideration the size of
+-- write the area of the triangle on the screen. Take into consideration the size of
 -- the font when positioning it on the screen.
-areaTextCircle = display.newText("The area of this circle with a radius of \n"..
-	radiusOfCircle .. " is " ..
-	areaOfCircle .. " pixels². ", 0, 50, Arial, textSize)
+areaTextTriangle = display.newText("The area of this triangle with a base of \n"..
+	baseOfTriangle .. " and a height of " .. heightOfTriangle .. " is " ..
+	areaOfTriangle .. " pixels². ", 250, 50, Arial, textSize)
+areaTextTriangle:setTextColor (0, 0, 0)
