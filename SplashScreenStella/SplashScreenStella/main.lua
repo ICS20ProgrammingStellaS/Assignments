c10bb192406eb24696cc9bcd0e73a35425fd0b62
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-11-11 17:48:43 -0500

    added

---
+++ SplashScreenStella/SplashScreenStella/main.lua
@@ -0,0 +1,18 @@
+-- Title: SplashScreen
+-- Name: Stella Armstrong
+-- Course: ICS2O
+-- This program...
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
