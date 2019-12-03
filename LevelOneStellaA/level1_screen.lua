1c0ca2ade1851534a969c8d7afa942c5e865bae0
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-12-03 14:26:15 -0500

    added

--- LevelOneStellaA/level1_screen.lua
+++ LevelOneStellaA/level1_screen.lua
@@ -40,6 +40,15 @@ local drop1
 local drop2
 local drop3
 
+-- question Images
+local ice
+local bruises
+local cuts
+local splinters
+local beeSting
+
+local randomOperator
+
 -- variables for the timer 
 local totalSeconds = 100
 local secondsLeft = 100
@@ -47,98 +56,8 @@ local clockText
 local countDownTimer  
 
 -----------------------------------------------------------------------------------------
--- GLOBAL SCENE FUNCTIONS
+-- LOCAL FUNCTIONS
 -----------------------------------------------------------------------------------------
-
--- The function called when the screen doesn't exist
-function scene:create( event )
-
-    -- Creating a group that associates objects with the scene
-    local sceneGroup = self.view
-
-    -----------------------------------------------------------------------------------------
-
-    -- Insert the background image
-    bkg_image = display.newImageRect("Images/Level1Screen.png", display.contentWidth, display.contentHeight)
-    bkg_image.x = display.contentCenterX
-    bkg_image.y = display.contentCenterY
-    bkg_image.width = display.contentWidth
-    bkg_image.height = display.contentHeight
-
-    -- Send the background image to the back layer so all other objects can be on top
-    bkg_image:toBack()
-
-        -- Insert background image into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( bkg_image )    
-
-end --function scene:create( event )
-
------------------------------------------------------------------------------------------
-
--- The function called when the scene is issued to appear on screen
-function scene:show( event )
-
-    -- Creating a group that associates objects with the scene
-    local sceneGroup = self.view
-    local phase = event.phase
-
-    -----------------------------------------------------------------------------------------
-
-    if ( phase == "will" ) then
-
-        -- Called when the scene is still off screen (but is about to come on screen).
-    -----------------------------------------------------------------------------------------
-
-    elseif ( phase == "did" ) then
-
-        -- Called when the scene is now on screen.
-        -- Insert code here to make the scene come alive.
-        -- Example: start timers, begin animation, play audio, etc.
-
-    end
-
-end --function scene:show( event )
-
------------------------------------------------------------------------------------------
-
--- The function called when the scene is issued to leave the screen
-function scene:hide( event )
-
-    -- Creating a group that associates objects with the scene
-    local sceneGroup = self.view
-    local phase = event.phase
-
-    -----------------------------------------------------------------------------------------
-
-    if ( phase == "will" ) then
-        -- Called when the scene is on screen (but is about to go off screen).
-        -- Insert code here to "pause" the scene.
-        -- Example: stop timers, stop animation, stop audio, etc.
-
-    -----------------------------------------------------------------------------------------
-
-    elseif ( phase == "did" ) then
-        -- Called immediately after scene goes off screen.
-    end
-
-end --function scene:hide( event )
-
------------------------------------------------------------------------------------------
-
--- The function called when the scene is issued to be destroyed
-function scene:destroy( event )
-
-    -- Creating a group that associates objects with the scene
-    local sceneGroup = self.view
-
-    -----------------------------------------------------------------------------------------
-
-    -- Called prior to the removal of scene's view ("sceneGroup").
-    -- Insert code here to clean up the scene.
-    -- Example: remove display objects, save state, etc.
-
-end -- function scene:destroy( event )
-
 
 -------------------------------------------------------------------------------------------------
 -- UPDATE TIMER
@@ -174,10 +93,10 @@ local function UpdateTime()
         elseif (lives == 1) then
             drop3.isVisible = false
         elseif (lives == 0) then -- if you lose all lives, make everything disappear
-            drop2.isVisible = false
-            drop1.isVisible = false
+            --drop2.isVisible = false
+            --drop1.isVisible = false
             clockText.isVisible = false
-            questionObject.isVisible = false
+            --questionObject.isVisible = false
 
             -- Go to the intro screen
             composer.gotoScene( "YouLose_screen" )
@@ -190,6 +109,35 @@ local function UpdateTime()
     end
 end 
 
+local function AskQuestion()
+    randomOperator = math.random (1, 2)
+
+    if (randomOperator == 1) then
+
+        -- correct answer
+        ice.isVisible = true
+
+        -- question
+        bruises.isVisible = true
+
+        -- wrong answers
+        cuts.isVisible = true
+        splinters.isVisible = true
+
+    elseif (randomOperator == 2) then
+
+        -- correct answer
+        cuts.isVisible = true
+
+        -- question
+        beeSting.isVisible = true
+
+        -- wrong answers
+        ice.isVisible = true
+        splinters.isVisible = true
+    end
+
+end
 -----------------------------------------------------------------------------------------------------------------------------
 
 -- function the calls the timer 
@@ -205,40 +153,166 @@ local function HideCorrect()
     AskQuestion()
 end
 
+
+
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------------------------------------------
+    -- OBJECT CREATION 
+    -----------------------------------------------------------------------------------------------------------------------------
+
+    -- Insert the background image
+    bkg_image = display.newImageRect("Images/Level1Screen.png", display.contentWidth, display.contentHeight)
+    bkg_image.x = display.contentCenterX
+    bkg_image.y = display.contentCenterY
+    bkg_image.width = display.contentWidth
+    bkg_image.height = display.contentHeight
+
+
+    -- display timer on screen
+    clockText = display.newText("", 100, 100, nil, 70)
+    clockText.x = display.contentWidth*2.8/4
+    clockText.y = display.contentHeight/8
+
+    -- create the lives to display on the screen 
+    drop1 = display.newImageRect("Images/redDrop.png", 100, 100)
+    drop1.x = display.contentWidth /7
+    drop1.y = display.contentHeight/2.5
+
+    drop2 = display.newImageRect("Images/redDrop.png", 100, 100)
+    drop2.x = display.contentWidth /11
+    drop2.y = display.contentHeight/3
+
+    drop3 = display.newImageRect("Images/redDrop.png", 100, 100)
+    drop3.x = display.contentWidth /5
+    drop3.y = display.contentHeight/3
+
+    -- questions 
+    bruises = display.newImageRect("Images for level one/Bruises.png", 230, 230)
+    bruises.x = display.contentWidth /2
+    bruises.y = display.contentHeight/1.7
+    --bruises.isVisible = false
+
+    cuts = display.newImageRect("Images for level one/Cuts.png", 190, 190)
+    cuts.x = display.contentWidth /2
+    cuts.y = display.contentHeight/1.16
+   -- cuts.isVisible = false
+
+    splinters = display.newImageRect("Images for level one/Slinters.png", 190, 190)
+    splinters.x = display.contentWidth /5
+    splinters.y = display.contentHeight/1.15
+    --splinters.isVisible = false
+
+    --answers
+    ice = display.newImageRect("Images for level one/IceBag.png", 190, 190)
+    ice.x = display.contentWidth /1.25
+    ice.y = display.contentHeight/1.2
+    --ice.isVisible = false
+
+    beeSting = display.newImageRect("Images for level one/Bee Sting.png", 200, 200)
+    beeSting.x = display.contentWidth /2
+    beeSting.y = display.contentHeight/1.7
+    --beeSting.isVisible = false
+
+
 -----------------------------------------------------------------------------------------------------------------------------
--- OBJECT CREATION 
------------------------------------------------------------------------------------------------------------------------------
 
--- display timer on screen
-clockText = display.newText("", 100, 100, nil, 70)
-clockText.x = display.contentWidth*2.8/4
-clockText.y = display.contentHeight/8
+    -- displays a question and sets the colour
+    questionObject = display.newText( " What tool do you use to help your patient? ", display.contentWidth/1.6, display.contentHeight/2.5, nil, 40)
+    questionObject:setTextColor(0, 102/255, 102/255)
 
--- create the lives to display on the screen 
-drop1 = display.newImageRect("Images/redDrop.png", 100, 100)
-drop1.x = display.contentWidth /7
-drop1.y = display.contentHeight/2.5
 
-drop2 = display.newImageRect("Images/redDrop.png", 100, 100)
-drop2.x = display.contentWidth /11
-drop2.y = display.contentHeight/3
+        -- Insert background image into the scene group in order to ONLY be associated with this scene
+    sceneGroup:insert( bkg_image ) 
+    sceneGroup:insert( drop1 )
+    sceneGroup:insert( drop2 )
+    sceneGroup:insert( questionObject ) 
 
-drop3 = display.newImageRect("Images/redDrop.png", 100, 100)
-drop3.x = display.contentWidth /5
-drop3.y = display.contentHeight/3
+end --function scene:create( event )
 
------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 
--- displays a question and sets the colour
-questionObject = display.newText( " What tool do you use to help your patient? ", display.contentWidth/1.6, display.contentHeight/2.5, nil, 40)
-questionObject:setTextColor(0, 102/255, 102/255)
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    if ( phase == "will" ) then
+
+        -- Called when the scene is still off screen (but is about to come on screen).
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+
+        -- Called when the scene is now on screen.
+        -- Insert code here to make the scene come alive.
+        -- Example: start timers, begin animation, play audio, etc.
+        StartTimer()
+
+    end
+
+end --function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    if ( phase == "will" ) then
+        -- Called when the scene is on screen (but is about to go off screen).
+        -- Insert code here to "pause" the scene.
+        -- Example: stop timers, stop animation, stop audio, etc.
+
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- Called immediately after scene goes off screen.
+        timer.cancel(countDownTimer)
+    end
+
+end --function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+
+end -- function scene:destroy( event )
+
+
 
 
 -----------------------------------------------------------------------------------------
 -- EVENT LISTENERS
 -----------------------------------------------------------------------------------------
 
-StartTimer()
 -- Adding Event Listeners
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
