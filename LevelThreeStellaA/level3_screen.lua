2209f4b0deafe12b248ffbb84557c960cc418eaa
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-12-27 15:49:56 -0500

    added

--- LevelThreeStellaA/level3_screen.lua
+++ LevelThreeStellaA/level3_screen.lua
@@ -32,10 +32,6 @@ local scene = composer.newScene( sceneName )
     -- The local variables for this scene
     local bkg_image
 
-    --local questionObject
-    --local nameObject
-    --local nameObject2
-
     -- lives 
     local lives = 3
     
@@ -52,10 +48,17 @@ local scene = composer.newScene( sceneName )
     local yell
     local ignore
     local tissue
+    local happy
+    local rude
+    local help
+    local laugh
 
     -- question Images
     local question1
     local question2
+    local question3
+    local question4
+    local question5
 
     local randomOperator
 
@@ -77,9 +80,16 @@ local function HideImages()
     question1.isVisible = false
     hug.isVisible = false
     question2.isVisible = false
+    question3.isVisible = false
+    question4.isVisible = false
+    question5.isVisible = false
     yell.isVisible = false
     ignore.isVisible = false
     tissue.isVisible = false
+    laugh.isVisible = false
+    help.isVisible = false
+    happy.isVisible = false
+    rude.isVisible = false
 end 
 
 local function ShowDrops()
@@ -177,11 +187,35 @@ function RemoveTouchListenersQ2L3()
     ignore:removeEventListener("touch", TouchListenerWrongAnswer1)
 end
 
+function RemoveTouchListenersQ3L3()
+    happy:removeEventListener("touch", TouchListenerAnswer)
+    tissue:removeEventListener("touch", TouchListenerWrongAnswer1)       
+    rude:removeEventListener("touch", TouchListenerWrongAnswer1)
+end
+
+function RemoveTouchListenersQ4L3()
+    help:removeEventListener("touch", TouchListenerAnswer)
+    yell:removeEventListener("touch", TouchListenerWrongAnswer1)       
+    ignore:removeEventListener("touch", TouchListenerWrongAnswer1)
+end
+
+function RemoveTouchListenersQ5L3()
+    help:removeEventListener("touch", TouchListenerAnswer)
+    yell:removeEventListener("touch", TouchListenerWrongAnswer1)       
+    laugh:removeEventListener("touch", TouchListenerWrongAnswer1)
+end
+
 function RemoveAllTouchListenersL3()
     if (randomOperator == 1) then
         RemoveTouchListenersQ1L3()
     elseif (randomOperator == 2) then
         RemoveTouchListenersQ2L3()
+    elseif (randomOperator == 3) then
+        RemoveTouchListenersQ3L3()
+    elseif (randomOperator == 4) then
+        RemoveTouchListenersQ4L3()
+    elseif (randomOperator == 5) then
+        RemoveTouchListenersQ5L3()
     end
 end
 
@@ -207,7 +241,7 @@ end
 
 
 function AskQuestionLevel3()
-    randomOperator = math.random (1, 2)
+    randomOperator = math.random (1, 5)
 
     incorrect.isVisible = false
     correct.isVisible = false
@@ -244,6 +278,54 @@ function AskQuestionLevel3()
         ignore.isVisible = true
         ignore:addEventListener("touch", TouchListenerWrongAnswer1)
 
+    elseif (randomOperator == 3) then
+        
+        -- question
+        question3.isVisible = true
+
+        -- correct answer
+        happy.isVisible = true
+        happy:addEventListener("touch", TouchListenerAnswer)
+
+        -- wrong answers
+        tissue.isVisible = true
+        tissue:addEventListener("touch", TouchListenerWrongAnswer1)
+
+        rude.isVisible = true
+        rude:addEventListener("touch", TouchListenerWrongAnswer1)
+
+    elseif (randomOperator == 4) then
+        
+        -- question
+        question4.isVisible = true
+
+        -- correct answer
+        help.isVisible = true
+        help:addEventListener("touch", TouchListenerAnswer)
+
+        -- wrong answers
+        yell.isVisible = true
+        yell:addEventListener("touch", TouchListenerWrongAnswer1)
+
+        ignore.isVisible = true
+        ignore:addEventListener("touch", TouchListenerWrongAnswer1)
+
+    elseif (randomOperator == 5) then
+        
+        -- question
+        question5.isVisible = true
+
+        -- correct answer
+        help.isVisible = true
+        help:addEventListener("touch", TouchListenerAnswer)
+
+        -- wrong answers
+        yell.isVisible = true
+        yell:addEventListener("touch", TouchListenerWrongAnswer1)
+
+        laugh.isVisible = true
+        laugh:addEventListener("touch", TouchListenerWrongAnswer1)
+
     end
 end
 
@@ -269,7 +351,7 @@ function scene:create( event )
     -----------------------------------------------------------------------------------------------------------------------------
 
     -- Insert the background image
-    bkg_image = display.newImageRect("Images for level one/backgroundL3.png", display.contentWidth, display.contentHeight)
+    bkg_image = display.newImageRect("Images for level one/BackGround.png", display.contentWidth, display.contentHeight)
     bkg_image.x = display.contentCenterX
     bkg_image.y = display.contentCenterY
     bkg_image.width = display.contentWidth
@@ -291,47 +373,83 @@ function scene:create( event )
     aidKit3.y = display.contentHeight/1.75
     aidKit3.isVisible = true
 
-    correct = display.newText("Correct!", display.contentWidth/2, display.contentHeight*3/4, nil, 75 )
+    correct = display.newText("Good Job!", display.contentWidth/2, display.contentHeight*3/4, nil, 75 )
     correct:setTextColor(153/255, 76/255, 0)
     correct.isVisible = false
 
-    incorrect = display.newText("Incorrect!", display.contentWidth/2, display.contentHeight*3/4, nil, 75 )
+    incorrect = display.newText("Oops wrong answer!", display.contentWidth/2, display.contentHeight*3/4, nil, 75 )
     incorrect:setTextColor(153/255, 76/255, 0)
     incorrect.isVisible = false
 
     -- questions 
-    question1 = display.newText("someone crying?", display.contentWidth/2, display.contentHeight/4.75, nil, 55 )
+    question1 = display.newText("someone is crying?", display.contentWidth/2, display.contentHeight/4.75, nil, 55 )
     question1:setTextColor(153/255, 76/255, 0)
     question1.isVisible = false
 
-    question2 = display.newText("someone was sad?", display.contentWidth/2, display.contentHeight/4.75, nil, 55 )
+    question2 = display.newText("someone is sad?", display.contentWidth/2, display.contentHeight/4.75, nil, 55 )
     question2:setTextColor(153/255, 76/255, 0)
     question2.isVisible = false
 
+    question3 = display.newText("someone is happy?", display.contentWidth/2, display.contentHeight/4.75, nil, 55 )
+    question3:setTextColor(153/255, 76/255, 0)
+    question3.isVisible = false
+
+    question4 = display.newText("someone is confused?", display.contentWidth/2, display.contentHeight/4.75, nil, 55 )
+    question4:setTextColor(153/255, 76/255, 0)
+    question4.isVisible = false
+
+    question5 = display.newText("someone is embarrased?", display.contentWidth/2, display.contentHeight/4.75, nil, 55 )
+    question5:setTextColor(153/255, 76/255, 0)
+    question5.isVisible = false
+
     --answers
     hug = display.newImageRect("Images for level one/Hug.png", 180, 170)
-    hug.x = display.contentWidth /1.35
+    hug.x = display.contentWidth /1.3
     hug.y = display.contentHeight/1.2
     hug.isVisible = false
     hug.name = "hug"
 
     yell = display.newImageRect("Images for level one/Yell.png", 180, 190)
-    yell.x = display.contentWidth /2
+    yell.x = display.contentWidth /1.95
     yell.y = display.contentHeight/1.2
     yell.isVisible = false
     yell.name = "yell"
 
     ignore = display.newImageRect("Images for level one/Ignore.png", 200, 180)
-    ignore.x = display.contentWidth /3.7
+    ignore.x = display.contentWidth /4
     ignore.y = display.contentHeight/1.2
     ignore.isVisible = false
     ignore.name = "ignore"
 
     tissue = display.newImageRect("Images for level one/GiveTissue.png", 200, 180)
-    tissue.x = display.contentWidth /3.7
+    tissue.x = display.contentWidth /4
     tissue.y = display.contentHeight/1.18
     tissue.isVisible = false
     tissue.name = "tissue"
+
+    happy = display.newImageRect("Images for level one/Behappytoo.png", 200, 180)
+    happy.x = display.contentWidth /1.95
+    happy.y = display.contentHeight/1.2
+    happy.isVisible = false
+    happy.name = "happy"
+
+    rude = display.newImageRect("Images for level one/Berude.png", 200, 180)
+    rude.x = display.contentWidth /1.3
+    rude.y = display.contentHeight/1.21
+    rude.isVisible = false
+    rude.name = "rude"
+
+    help = display.newImageRect("Images for level one/Helpthem.png", 200, 180)
+    help.x = display.contentWidth /1.3
+    help.y = display.contentHeight/1.2
+    help.isVisible = false
+    help.name = "help"
+
+    laugh = display.newImageRect("Images for level one/Laughatthem.png", 230, 190)
+    laugh.x = display.contentWidth /4
+    laugh.y = display.contentHeight/1.2
+    laugh.isVisible = false
+    laugh.name = "laugh"
 
 
 
@@ -346,10 +464,17 @@ function scene:create( event )
     sceneGroup:insert( incorrect )
     sceneGroup:insert( question1 )
     sceneGroup:insert( question2 )
+    sceneGroup:insert( question3 )
+    sceneGroup:insert( question4 )
+    sceneGroup:insert( question5 )
     sceneGroup:insert( hug )
     sceneGroup:insert( yell )
     sceneGroup:insert( ignore )
     sceneGroup:insert( tissue )
+    sceneGroup:insert( help )
+    sceneGroup:insert( rude )
+    sceneGroup:insert( happy )
+    sceneGroup:insert( laugh )
 
 end --function scene:create( event )
 
